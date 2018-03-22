b121d9801d0b4dff8af3dec10d41f4771b1d908d
LeoMoonStar <lostmoonstar@gmail.com>
2018-03-22 19:27:11 -0400

    Add files via upload

---
+++ chatclient.c
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: zhan2500
+ *
+ * Created on March 18, 2018, 2:05 PM
+ */
+
+/*
+ ** selectserver.c -- a cheezy multiperson chat server
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define PORT "9034"   // port we're listening on
+
+// get sockaddr, IPv4 or IPv6:
+
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*) sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*) sa)->sin6_addr);
+}
+
+int main(void) {
+    fd_set master; // master file descriptor list
+    fd_set read_fds; // temp file descriptor list for select()
+    int fdmax; // maximum file descriptor number
+
+    int listener; // listening socket descriptor
+    int newfd; // newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // client address
+    socklen_t addrlen;
+
+    char buf[256]; // buffer for client data
+    int nbytes;
+
+    char remoteIP[INET6_ADDRSTRLEN];
+
+    int yes = 1; // for setsockopt() SO_REUSEADDR, below
+    int i, j, rv;
+    int sockfd;
+
+    struct addrinfo hints, *ai, *p, *servinfo;
+
+    FD_ZERO(&master); // clear the master and temp sets
+    FD_ZERO(&read_fds);
+
+    // get us a socket and bind it
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+        exit(1);
+    }
+    
+    
+
+   
+    if (p == NULL) {
+        fprintf(stderr,"talker: failed to create socket\n");
+        return 2;
+    }
+
+    for (p = ai; p != NULL; p = p->ai_next) {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener < 0) {
+            continue;
+        }
+
+        // lose the pesky "address already in use" error message
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (int));
+
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+            close(listener);
+            continue;
+        }
+
+        break;
+    }
+
+    // if we got here, it means we didn't get bound
+    if (p == NULL) {
+        fprintf(stderr, "selectserver: failed to bind\n");
+        exit(2);
+    }
+
+    freeaddrinfo(ai); // all done with this
+
+    // listen
+    if (listen(listener, 10) == -1) {
+        perror("listen");
+        exit(3);
+    }
+
+    // add the listener to the master set
+    FD_SET(listener, &master);
+
+    // keep track of the biggest file descriptor
+    fdmax = listener; // so far, it's this one
+
+    printf("fdmax: %d", fdmax);
+    
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+    
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+    
+    
+    // what to send 
+    char input[500];
+    int numbytes;
+
+    // main loop
+    for (;;) {
+        read_fds = master; // copy it
+        if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(4);
+        }
+
+        // run through the existing connections looking for data to read
+        for (i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { // we got one!!
+                if (i == 0) { //if this is keyboard input
+                    fgets(input, 500, stdin);
+                    //send data to port
+                    if ((numbytes = sendto(sockfd, input, strlen(input), 0,
+                            p->ai_addr, p->ai_addrlen)) == -1) {
+                        perror("talker: sendto");
+                        exit(1);
+                    }
+
+                } else {
+                    // handle data from a client
+                    if ((nbytes = recv(sockfd, buf, sizeof buf, 0)) <= 0) {
+                        // got error or connection closed by client
+                        if (nbytes == 0) {
+                            // connection closed
+                            printf("selectserver: socket %d hung up\n", i);
+                        } else {
+                            perror("recv");
+                        }
+                        close(i); // bye!
+                        FD_CLR(i, &master); // remove from master set
+                    } else {
+                        // we got some data from a client
+                        printf("%s\n",buf);
+                    }
+                } // END handle data from client
+            } // END got new incoming connection
+        } // END looping through file descriptors
+    } // END for(;;)--and you thought it would never end!
+
+    return 0;
+}
